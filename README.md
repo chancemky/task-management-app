67d0ce83e1e9b55672561a156b7f23e786706493
chancemky <chancemky@gmail.com>
2024-06-15 01:02:41 -1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# task-management-app
+# Task Management Application: <a href="https://666d69f856f07cad8b1b0482--dashing-dasik-a8b086.netlify.app/" target="_blank">Visit Here</a>
+<img src="assets/website-image.png" width="100%" alt="Task Management Application"/>
+</a>
+This is a Task Management Application that allows users to input tasks and display them in a list format with options to edit or delete each task. Tasks are stored locally using localStorage, ensuring persistence across page reloads. Key features include dynamic UI updates upon user actions (add, edit, delete tasks) and responsive design for mobile and desktop views.
+
+## Tech used: 
+![HTML5 BADGE](https://img.shields.io/static/v1?label=|&message=HTML5&color=23555f&style=plastic&logo=html5)![CSS BADGE](https://img.shields.io/static/v1?label=|&message=CSS3&color=285f65&style=plastic&logo=css3)![JAVASCRIPT BADGE](https://img.shields.io/static/v1?label=|&message=JAVASCRIPT&color=3c7f5d&style=plastic&logo=javascript)
+
+## Optimizations
+
+<li>Implement asynchronous data handling (e.g., AJAX) to update the UI dynamically without full page refreshes.</li>
+<li>Add ARIA attributes and roles to enhance screen reader compatibility and keyboard navigation.</li>
+<li>Validate user inputs and handle potential errors (e.g., empty tasks) with informative messages or UI cues.</li>
+<li>Test and refine CSS media queries to improve layout consistency and usability on different devices</li>
+
+## Lessons Learned:
+
+<li>Leveraging localStorage for storing application data locally across sessions.</li>
+<li>Managing user interactions through event listeners efficiently.
+</li>
+<li>Updating the DOM dynamically based on user actions.</li>
+<li>Using CSS Flexbox for responsive and flexible layout designs.</li>
+<li>Designing intuitive user interfaces with interactive elements (buttons, input fields).</li>
---
+++ main.js
@@ -0,0 +1,107 @@
+const itemsArray = JSON.parse(localStorage.getItem('items')) || []; // Retrieve items from localStorage or initialize an empty array if none exists
+
+document.querySelector("#enter").addEventListener("click", () => { // Add event listener for click on #enter button
+    const item = document.querySelector("#item");
+    createItem(item); // Call createItem function with the entered item
+});
+
+document.querySelector("#item").addEventListener("keypress", (e) => { // Add event listener for keypress in #item input
+    if(e.key === "Enter") {
+        const item = document.querySelector("#item");
+        createItem(item); // Call createItem function if Enter key is pressed
+    }
+});
+
+function displayDate() {
+    const date = new Date().toString().split(" ").slice(1, 4).join(" "); // Format current date
+    document.querySelector("#date").innerHTML = date; // Display formatted date
+}
+
+function displayItems() {
+    let items = "";
+    itemsArray.forEach((item) => { // Loop through itemsArray to create HTML for each item
+        items += `<div class="item">
+                      <div class="input-controller">
+                          <textarea disabled>${item}</textarea>
+                          <div class="edit-controller">
+                              <i class="fa-solid fa-check deleteBtn"></i>
+                              <i class="fa-solid fa-pen-to-square editBtn"></i>
+                          </div>
+                      </div>
+                      <div class="update-controller">
+                          <button class="saveBtn">Save</button>
+                          <button class="cancelBtn">Cancel</button>
+                      </div>
+                  </div>`;
+    });
+    document.querySelector(".task-list").innerHTML = items; // Display items in .task-list container
+    activateDeleteListeners(); // Activate delete button listeners
+    activateEditListeners(); // Activate edit button listeners
+    activateSaveListeners(); // Activate save button listeners
+    activateCancelListeners(); // Activate cancel button listeners
+}
+
+function activateDeleteListeners() {
+    const deleteBtn = document.querySelectorAll(".deleteBtn");
+    deleteBtn.forEach((dB, i) => {
+        dB.addEventListener("click", () => { deleteItem(i); }); // Add click event listener to each delete button
+    });
+}
+
+function activateEditListeners() {
+    const editBtn = document.querySelectorAll(".editBtn");
+    const updateController = document.querySelectorAll(".update-controller");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    editBtn.forEach((eB, i) => {
+        eB.addEventListener("click", () => { 
+            updateController[i].style.display = "block"; // Display update controls
+            inputs[i].disabled = false; // Enable textarea for editing
+        });
+    });
+}
+
+function activateSaveListeners() {
+    const saveBtn = document.querySelectorAll(".saveBtn");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    saveBtn.forEach((sB, i) => {
+        sB.addEventListener("click", () => {
+            updateItem(inputs[i].value, i); // Call updateItem with new value on save button click
+        });
+    });
+}
+
+function activateCancelListeners() {
+    const cancelBtn = document.querySelectorAll(".cancelBtn");
+    const updateController = document.querySelectorAll(".update-controller");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    cancelBtn.forEach((cB, i) => {
+        cB.addEventListener("click", () => {
+            updateController[i].style.display = "none"; // Hide update controls
+            inputs[i].disabled = true; // Disable textarea
+            inputs[i].style.border = "none"; // Remove border
+        });
+    });
+}
+
+function createItem(item) {
+    itemsArray.push(item.value); // Add new item to itemsArray
+    localStorage.setItem('items', JSON.stringify(itemsArray)); // Update localStorage with updated itemsArray
+    location.reload(); // Reload the page to display updated list
+}
+
+function deleteItem(i) {
+    itemsArray.splice(i, 1); // Remove item at index i from itemsArray
+    localStorage.setItem('items', JSON.stringify(itemsArray)); // Update localStorage with updated itemsArray
+    location.reload(); // Reload the page to display updated list
+}
+
+function updateItem(text, i) {
+    itemsArray[i] = text; // Update item at index i in itemsArray
+    localStorage.setItem('items', JSON.stringify(itemsArray)); // Update localStorage with updated itemsArray
+    location.reload(); // Reload the page to display updated list
+}
+
+window.onload = function() {
+    displayDate(); // Display current date
+    displayItems(); // Display items stored in localStorage on page load
+};
